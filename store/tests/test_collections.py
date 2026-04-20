05e0e0a6ede3e9f1703db9e2421f7b42288d3bfe
warsana <inyomanwarsana1@gmail.com>
2026-04-21 02:00:15 +0800

    Creating model instances

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,6 +1,8 @@
+from store.models import Collection
 from django.contrib.auth.models import User
 from rest_framework import status
 import pytest
+from model_bakery import baker
 
 
 
@@ -38,3 +40,17 @@ class TestCreateCollection:
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, api_client):
+        collection = baker.make(Collection)
+        response = api_client.get(f'/store/collections/{collection.id}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
