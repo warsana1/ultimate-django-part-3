3c0eda167eaf256469952141c55e5649377f3fd3
warsana <inyomanwarsana1@gmail.com>
2026-04-22 16:26:30 +0800

    Serving static assets

--- storefront/settings.py
+++ storefront/settings.py
@@ -55,8 +55,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
